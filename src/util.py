5720bfdb4d3e9bed91cf81020b012540fd5c5b3d
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-04-03 22:18:31 +0700

    Menghilangkan fungsi yang tidak dipakai

--- src/util.py
+++ src/util.py
@@ -2,12 +2,15 @@ import random
 
 def txtToArray(fileName):
     # Reads a text file and returns an array of number
+    # Inisiasi array
     arrayOfNum = []
     with open(fileName, 'r') as f:
         lines = f.readlines()
         for line in lines:
+            # Pisah string berdasarkan spasi
             line = line.split(" ")
             for char in line:
+                # Append ke array dalam bentuk integer
                 if char != '\n' and char != ' ':
                     arrayOfNum.append(int(char))
     return arrayOfNum
@@ -16,14 +19,9 @@ def randPuzzle():
     # Returns a random puzzle
     puzzle = []
     while len(puzzle) < 16:
+        # Generate angka random 0 hingga 15
         num = random.randint(0,15)
+        # Jika angka belum ada di puzzle, append
         if num not in puzzle:
             puzzle.append(num)
     return puzzle
-
-def hasPuzzleGotChecked(puzzle, listOfSimpulEkspan):
-    # Returns True if the puzzle is in list of Simpul Ekspan
-    for puzzle2 in listOfSimpulEkspan:
-        if puzzle.isEqual(puzzle2):
-            return True
-    return False
